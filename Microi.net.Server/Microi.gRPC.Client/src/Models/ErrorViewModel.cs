61c7ac91df20540e661668fe0bd2bfed2b18b285
Anderson <admin@itdos.com>
2025-03-01 17:05:13 +0800

    .net gRPC客户端演示代码提交

---
+++ Microi.net.Server/Microi.gRPC.Client/src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Microi.net.Grpc.Client.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
