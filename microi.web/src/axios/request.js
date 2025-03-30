e6c59068d91ce03c5a302c12d592424658f20871
Anderson <admin@itdos.com>
2025-03-30 17:05:38 +0800

    * 新增控件数据源可选择接口引擎配置 * 修复控件数据源为接口引擎、数据源引擎时首次不加载数据的bug

--- microi.web/src/axios/request.js
+++ microi.web/src/axios/request.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-  // baseURL: 'https://api-china.itdos.com', // 接口基础路径
-  baseURL: 'https://api.nbweixin.cn',
+  baseURL: 'https://api-china.itdos.com', // 接口基础路径
   timeout: 20000, // 请求超时时间，默认20秒
 })
 
