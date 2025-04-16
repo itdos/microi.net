580897ae644d4a3d477718689700ba9564baa21d
李赛赛 <359681091@qq.com>
2025-04-16 14:40:14 +0800

    优化了界面引擎统计组件，添加了子项页面跳转路径

---
+++ microi.web/public/autopage/assets/image-widget-Dlc12qfX.js
@@ -0,0 +1,1 @@
+import{l as a,b as i,e as o}from"./index-wPmqCD4D.js";const n={__name:"image-widget",props:{widgetObj:{type:Object,required:!0}},setup(t){return(s,c)=>{var e;const r=a("el-image");return i(),o(r,{src:(e=t.widgetObj.widgetParams[0])==null?void 0:e.value,fit:"fill",style:{width:"100%",height:"100%"}},null,8,["src"])}}};export{n as default};
