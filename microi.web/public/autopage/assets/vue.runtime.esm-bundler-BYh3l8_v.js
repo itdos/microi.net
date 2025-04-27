452d793690d01adf99000e09819d0a9b1b33ca52
李赛赛 <359681091@qq.com>
2025-04-27 17:31:24 +0800

    优化了界面引擎区域地图点击触发事件跳转路由

--- microi.web/public/autopage/assets/vue.runtime.esm-bundler-BYh3l8_v.js
+++
@@ -1,1 +0,0 @@
-import{Q as e,R as r,S as s}from"./index-Cd0LJEJn.js";const t=e(r),m=e(s);export{m as a,t as r};
---
+++ microi.web/public/autopage/assets/vue.runtime.esm-bundler-DjFZiaO1.js
@@ -0,0 +1,1 @@
+import{Q as e,R as r,S as s}from"./index-MpZPmrPb.js";const t=e(r),m=e(s);export{m as a,t as r};
