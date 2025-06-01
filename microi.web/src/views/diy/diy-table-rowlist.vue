786919d24631958dd2356b2ebc6ed77b01356ae2
liucheng <10368826@qq.com>
2025-06-01 16:18:15 +0800

    删除按钮点击，页面卡死，已处理

--- microi.web/src/views/diy/diy-table-rowlist.vue
+++ microi.web/src/views/diy/diy-table-rowlist.vue
@@ -4568,6 +4568,7 @@ export default {
           return false;
         }
       }
+      return;
     },
     //离开表单动作
     async FormOutAction(actionType, submitAfterType, tableRowId, V8Callback, rowModel) {
