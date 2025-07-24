8f31710fb67c9e0487ff83927dd86cf31a5ccec4
cuisimin <2442979539@qq.com>
2025-07-24 16:13:27 +0800

    根据标识决定是否执行表单进入事件（子表提交后不执行）

--- microi.uniapp.uni-ui/static/config/index.js
+++ microi.uniapp.uni-ui/static/config/index.js
@@ -3,6 +3,7 @@ var configOsClient = {
   loctek: 'https://lowcode-center-api.loctek.com',
   iTdos: 'https://api-china.itdos.com',
   dahongcrm: 'http://121.41.225.44:1012', //'https://crm_api.nbdahong.com', //
-  shangwei: 'https://api.cn-sunway.com'
+  shangwei: 'https://api.cn-sunway.com',
+  zhongfu: 'https://api.sinofoauto.com' // 中弗
 }
 export default configOsClient
