acc309248c2d4188f127b756cd4e1c212844fb7e
Anderson <admin@itdos.com>
2026-01-23 04:37:47 +0800

    vue2传统界面升级至vue3前，删除不再使用的文件。

--- microi.web/src/views/custom/jiejie/index.js
+++
@@ -1,1 +0,0 @@
-export { default as RegionMap } from "./RegionMap.vue";
--- microi.web/src/views/plugins/test-plugin/window.js
+++
@@ -1,9 +0,0 @@
-window["loading"] = {
-    show: function () {
-        console.log("show loading");
-        return "show loading";
-    },
-    hide: function () {
-        console.log("hide loading");
-    }
-};
