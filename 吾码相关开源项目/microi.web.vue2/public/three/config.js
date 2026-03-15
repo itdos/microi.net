5d5f1ccc0f7a6d347098aa915971cddba010e737
Anderson・周 <973702@qq.com>
2026-03-15 16:47:50 +0800

    删除无用大文件

--- 吾码相关开源项目/microi.web.vue2/public/three/config.js
+++
@@ -1,7 +0,0 @@
-// /public/config.js
-window.APP_CONFIG = {
-  API_MAP_IMAGE_LIST: "https://api.nbweixin.cn/apiengine/three_map?OsClient=empty",
-  API_BACKGROUND_LIST: "https://api.nbweixin.cn/apiengine/three_bgimg?OsClient=empty",
-  API_VIEW_IMAGE_LIST: "https://api.nbweixin.cn/apiengine/three_bgimg?OsClient=empty",
-  API_MODEL_LIST: "https://api.nbweixin.cn/apiengine/three_model?OsClient=empty"
-};
